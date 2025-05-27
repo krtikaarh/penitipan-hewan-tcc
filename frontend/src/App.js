fd21e8ba1f1c646bd4888f0476300a5b5177e031
Kartika Rahmi <kartikarahmi7@gmail.com>
2025-05-27 09:49:51 +0700

    edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import AxiosInterceptor from './api/axiosInterceptor';
+
 
 import Login from './components/Login';
 import Register from './components/Register';
@@ -13,7 +13,7 @@ import Home from './components/Home';
 function App() {
   return (
     <>
-    <AxiosInterceptor />
+
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
