7e8952934f8e8cdc085234565c9e321b788ba540
Kartika Rahmi <kartikarahmi7@gmail.com>
2025-06-01 17:15:32 +0700

    edit

--- backend/middleware/VerifyToken.js
+++ backend/middleware/VerifyToken.js
@@ -4,10 +4,10 @@ export const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) return res.sendStatus(401); // artinya token tidak ada
+  if (token == null) return res.sendStatus(401); 
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-    if (error) return res.sendStatus(403); // Forbidden
+    if (error) return res.sendStatus(403); 
     req.user = decoded; 
     next();
   });
